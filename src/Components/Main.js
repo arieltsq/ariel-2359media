91fdffa5cb300eb102f1bcf6756c5f76b347bc8f
Ariel Tay <shihqian@gmail.com>
2017-09-23 18:03:55 +0800

    editing the routes. seems to have issue routing on production

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import { Switch, Route} from 'react-router-dom'
+import {Route,BrowserRouter} from 'react-router-dom'
+
 import SearchImages from './SearchImages'
 import Favourites from './Favourites'
 
 // Acts like routes
 
 const Main = () => (
-  <div>
-    <Switch>
-      <Route exact path="/" component={SearchImages}/>
-      <Route   path="/" component={SearchImages}/>
-      <Route path="/favorites" component={Favourites}/>
-    </Switch>
-  </div>
+  <BrowserRouter>
+    <div>
+            <Route exact path="/" component={SearchImages}/>
+            <Route   path="/search" component={SearchImages}/>
+            <Route path="/favorites" component={Favourites}/>
+     </div>
+   </BrowserRouter>
+
 )
 
 export default Main
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
 render((
-  <BrowserRouter>
-
     <App />
-  </BrowserRouter>
 ), document.getElementById('root'));
